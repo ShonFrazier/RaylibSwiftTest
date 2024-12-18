ff986c4866cac8d4df0f262900f8643b13df1297
Shon Frazier <code@shon.io>
2024-12-18 09:30:28 -0500

    Initial Commit

---
+++ Yokta Beginnings/Yokta_BeginningsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yokta_BeginningsApp.swift
+//  Yokta Beginnings
+//
+//  Created by Shon on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Yokta_BeginningsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
